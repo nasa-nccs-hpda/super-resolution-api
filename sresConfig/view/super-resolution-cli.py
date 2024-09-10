dd96c3fe0214cab5e93c1e42a35d981c64df6c02
gtamkin <J3rgensen9!>
2024-09-10 16:28:44 -0400

    Insert super-resolution-climate-09042024

--- sresConfig/view/super-resolution-cli.py
+++ sresConfig/view/super-resolution-cli.py
@@ -16,6 +16,12 @@ import sys
 
 # import veto.config
 # sys.modules["sres.base.util.config"] = veto.config
+sys.path.insert(0,'.')
+sys.path.insert(1,'./veto')
+sys.path.insert(2,'./super-resolution-climate')
+sys.path.insert(3,'../../veto')
+sys.path.insert(4,'../../super-resolution-climate')
+print('\n', sys.path)
 
 import veto.gpu
 import veto.dual_trainer
