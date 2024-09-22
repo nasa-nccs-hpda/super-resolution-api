9399dfecb80caa046db8de88e6e9416d123dc7d6
gtamkin <J3rgensen9!>
2024-09-22 14:49:43 -0400

    tightened up

--- sresConfig/view/super-resolution-cli.py
+++ sresConfig/view/super-resolution-cli.py
@@ -1,35 +1,22 @@
 """
-Purpose: Build and apply regression model for coefficient identification of raster data using
-         low resolution data (~30m) and TARGET inputs. Apply the coefficients to high resolution data (~2m)
-         to generate a surface reflectance product (aka, SR-Lite). Usage requirements are referenced in README.
-
-Data Source: This script has been tested with very high-resolution WV data.
-             Additional testing will be required to validate the applicability
-             of this model for other datasets.
-
-Original Author: Glenn Tamkin, CISTO, Code 602
+Super Resolution Application Programming Interface (API) for Weather/Climate Data Framework.
 """
 # --------------------------------------------------------------------------------
 # Import System Libraries
 # --------------------------------------------------------------------------------
 import sys
 
+
 import veto.gpu
 import veto.workflow
 sys.modules["sres.base.gpu"] = veto.gpu
 sys.modules["sres.controller.workflow"] = veto.workflow
 
-from typing import Any, Dict, List, Tuple, Type, Optional, Union
+from typing import Any, Dict, List
 import time  # tracking time
-from pathlib import Path
 
 from sresConfig.model.parms import parms
 from sresConfig.controller.actions import ActionController
-
-from omegaconf import DictConfig
-from hydra.core.global_hydra import GlobalHydra
-import hydra
-
 
 def main():
     """
@@ -83,51 +70,4 @@ def main():
 
 
 if __name__ == "__main__":
-    from unittest.mock import patch
-
-    start_time = time.time()  # record start time
-
-    maindir = '/adapt/nobackup/projects/ilab/data/srlite'
-
-    r_fn_ccdc = \
-        '/panfs/ccds02/nobackup/people/iluser/projects/srlite/test/input/baseline/WV02_20150911_M1BS_1030010049148A00-ccdc.tif'
-    r_fn_evhr = \
-        '/panfs/ccds02/nobackup/people/iluser/projects/srlite/test/input/baseline/WV02_20150911_M1BS_1030010049148A00-toa.tif'
-    r_fn_cloud = \
-        '/panfs/ccds02/nobackup/people/iluser/projects/srlite/test/input/baseline/WV02_20150911_M1BS_1030010049148A00-toa.cloudmask.v1.2.tif'
-
-
-    # If not arguments specified, use the defaults
-    numParms = len(sys.argv) - 1
-    if numParms == 0:
-
-        with patch("sys.argv",
-
-                   ["SrliteWorkflowCommandLineView.py",
-                    "-toa_dir", r_fn_evhr,
-                    "-target_dir", r_fn_ccdc,
-                    "-cloudmask_dir", r_fn_cloud,
-                    "-bandpairs",
-                    #"[['blue_ccdc', 'BAND-B'],['green_ccdc','BAND-G'],['red_ccdc','BAND-R'],['nir_ccdc','BAND-N']]",
-                    #                "-bandpairs", "[['BAND-B', 'blue_ccdc'], ['BAND-G', 'green_ccdc'],
-                    #                ['BAND-R', 'red_ccdc'], ['BAND-N', 'nir_ccdc']]",
-                    "[['blue_ccdc', 'BAND-B'], ['green_ccdc', 'BAND-G'], ['red_ccdc', 'BAND-R'], ['nir_ccdc', 'BAND-N'], ['blue_ccdc', 'BAND-C'], ['green_ccdc', 'BAND-Y'], ['red_ccdc', 'BAND-RE'], ['nir_ccdc', 'BAND-N2']]",
-                    "-output_dir", "/explore/nobackup/people/gtamkin/dev/srlite/test/v2_srlite-2.0-rma-baseline/20240305-cantfingbelieveit",
-                    "--debug", "1",
-                    "--regressor", "rma",
-                    "--clean",
-                    "--cloudmask",
-                    "--pmask",
-                    "--csv",
-                    ]):
-            ##############################################
-            # main() using default application parameters
-            ##############################################
-            print("Default application parameters: {sys.argv}")
-            main()
-    else:
-
-        ##############################################
-        # main() using command line parameters
-        ##############################################
-        main()
+     main()
